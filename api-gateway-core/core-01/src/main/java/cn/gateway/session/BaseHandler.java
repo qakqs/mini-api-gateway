1cc1d4e3e23ba535206edb3c9a95513fbe6e1d3b
qakqs <1248001568@qq.com>
2023-07-13 21:38:12 +0800

    111

--- api-gateway-core/core-01/src/main/java/cn/gateway/session/BaseHandler.java
+++ api-gateway-core/core-01/src/main/java/cn/gateway/session/BaseHandler.java
@@ -7,6 +7,8 @@ import io.netty.channel.SimpleChannelInboundHandler;
 public abstract class BaseHandler<T> extends SimpleChannelInboundHandler<T> {
     @Override
     protected void channelRead0(ChannelHandlerContext channelHandlerContext, T msg) throws Exception {
+
+
         session(channelHandlerContext, channelHandlerContext.channel(), msg);
     }
 
